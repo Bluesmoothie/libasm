86a6e0c875007017ff3f06cb2a69f497b5ea0841
Yves Gille <ygille@student.42lyon.fr>
2025-07-06 01:53:09 +0200

    strcpy works too

--- src/main.c
+++ src/main.c
@@ -4,6 +4,12 @@
 int	main(void)
 {
 	const char*	str = "Hello World!";
-	printf("%s = %zu", str, ft_strlen(str));
+	const char*	s1 = "abc";
+	const char* s2 = "abd";
+	char		cpy[12];
+	printf("%s = %zu\n", str, ft_strlen(str));
+	printf("%d\n", ft_strcmp(s1, s2));
+	ft_strcpy(cpy, str, 13);
+	printf("%s\n", cpy);
 	return 0;
 }
