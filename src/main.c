1ab8d95b3f9b203ebf5137dd9a830f5aa5143ed2
Yves Gille <ygille@student.42lyon.fr>
2025-07-06 02:01:05 +0200

    read and write functionnal

--- src/main.c
+++ src/main.c
@@ -7,9 +7,13 @@ int	main(void)
 	const char*	s1 = "abc";
 	const char* s2 = "abd";
 	char		cpy[12];
+	char		buff[1024];
+	ssize_t		len;
 	printf("%s = %zu\n", str, ft_strlen(str));
 	printf("%d\n", ft_strcmp(s1, s2));
 	ft_strcpy(cpy, str, 13);
 	printf("%s\n", cpy);
+	len = ft_read(STDIN_FILENO, buff, 1024);
+	ft_write(STDOUT_FILENO, buff, len);
 	return 0;
 }
